6fb77c48414a965c3aa2263590619646630cb453
Nadia Bahrami <nadia.bahrami@gmail.com>
2016-03-07 20:47:09 -0800

    Minor spacing fixed.

--- src/client.py
+++ src/client.py
@@ -9,22 +9,16 @@ def client_send(message):
     client = socket.socket(*stream_info[:3])
     client.connect(stream_info[-1])
     client.sendall(message.encode('utf8'))
-
     buffer_length = 8
     reply_complete = False
     full_string = ""
     while not reply_complete:
         part = client.recv(buffer_length)
         full_string = full_string + part.decode('utf8')
-        if len(part) < buffer_length:  #add a time out or a zero byte push
+        if len(part) < buffer_length:
             reply_complete = True
             client.close()
     print(full_string)
-
-
-# def client(message):
-#     client_send(message):
-#     print(client_receive())#is there an internal parameter?
 
 
 if __name__ == '__main__':
