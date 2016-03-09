cee878ca5c49076cff8adb3d4c12919a7c973ca1
Daniel Zwelling <dzwellingmusic@gmail.com>
2016-03-08 16:21:59 -0800

    Server working. Need to fix date. Response not loading

--- src/server.py
+++ src/server.py
@@ -2,12 +2,42 @@
 """Server module."""
 from __future__ import unicode_literals
 import socket
+import email.utils
+
+
+def response_ok(body):
+    """Return 200 ok."""
+    first_line = 'HTTP/1.1 200 OK'
+    second_line = 'Content-Type: text/plain; charset=utf-8'
+    # date_line = 'Date: {}'.format(email.utils.formatdate(usegmt=True))
+    header_break = '\r\n'
+    bytes = body.encode('utf-8')
+    fourth_line = 'Content-Length: {}'.format(len(bytes))
+    body_message = 'Body: It is all good'
+    string_list = [first_line, second_line, fourth_line, header_break, body_message]
+    string_list = '\r\n'.join(string_list)
+    return string_list
+
+
+def response_error(body):
+    """Return 500 internal server error."""
+    first_line = 'HTTP/1.1 500 Internal Server Error'
+    second_line = 'Content-Type: text/plain; charset=utf-8'
+    # date_line = email.utils.formatdate(usegmt=True)
+    header_break = '<CRLF>'
+    bytes = body.encode('utf-8')
+    fourth_line = 'Content-Length: {}'.format(len(bytes))
+    body_message = 'Body: The system is down'
+    string_list = [first_line, second_line, fourth_line, header_break, body_message]
+    string_list = '\r\n'.join(string_list)
+    return string_list
 
 
 def server():
     """Return message to client."""
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
     address = ('127.0.0.1', 5000)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server.bind(address)
     server.listen(1)
     conn, addr = server.accept()
@@ -15,17 +45,23 @@ def server():
         while True:
             buffer_length = 8
             reply_complete = False
-            full_string = ""
+            full_string = u""
             while not reply_complete:
                 part = conn.recv(buffer_length)
                 full_string = full_string + part.decode('utf-8')
-                if len(part) < buffer_length:  #add a time out or a zero byte push
+                if len(part) < buffer_length:
                     reply_complete = True
             print(full_string)
-            conn.sendall(full_string.encode('utf-8'))
+            body = full_string
+            print(response_ok(body))
+            conn.sendall(response_ok(body).encode('utf-8'))
             server.listen(1)
             conn, addr = server.accept()
     except KeyboardInterrupt:
     	conn.close()
     	server.close()
 server()
+
+
+
+
